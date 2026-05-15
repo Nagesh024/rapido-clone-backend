6cb40d032f7802697c237e49340116400ed4a299
Nagesh024 <167857550+Nagesh024@users.noreply.github.com>
2026-05-15 13:44:14 +0530

    Complete notification-service with websocket and docker

---
+++ notification-service/src/main/java/com/rapido/notificationservice/dto/NotificationEvent.java
@@ -0,0 +1,52 @@
+package com.rapido.notificationservice.dto;
+
+import com.rapido.notificationservice.entity.NotificationType;
+
+public class NotificationEvent {
+
+    private Long userId;
+    private NotificationType type;
+    private String recipient;
+    private String title;
+    private String message;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
