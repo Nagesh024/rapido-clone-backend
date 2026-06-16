7428535d3931939c46580f8662fc1028b5c7abd9
Nagesh024 <167857550+Nagesh024@users.noreply.github.com>
2026-06-16 20:13:49 +0530

    Add files via upload

---
+++ performance-service/src/main/java/com/rapido/performanceservice/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.rapido.performanceservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
---
+++ performance-service/src/main/java/com/rapido/performanceservice/load/LoadRunnerApplication.java
@@ -0,0 +1,14 @@
+//package com.rapido.performanceservice.load;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class LoadRunnerApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(
+//                LoadRunnerApplication.class,
+//                args);
+//    }
+//}
